b972e49ccdeb998516263fff199261876ec23748
M.F.M Fazrin <mfmfazrin1986@gmail.com>
2019-10-09 02:17:07 +0530

    --- Auto-Git Commit ---

--- SalesMan.cs
+++ SalesMan.cs
@@ -4,6 +4,17 @@ using System.Text;
 
 public abstract class SalesMan
 {
+    private const string format = "{0} {1}";
+
+    private string _firstName { get; set; }
+    private string _lastName { get; set; }
+
+    public string FullName
+    {
+     get{
+            return string.Format(format, _firstName + _lastName);
+        }
+    }
 
 }
 
