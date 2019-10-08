e7e7aa22ad8bd6852940192e6e3b519741a27e10
M.F.M Fazrin <mfmfazrin1986@gmail.com>
2019-10-09 02:02:29 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOP_IN_C_Sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
